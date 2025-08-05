f8737f796b7526a98cec8d9e21058ae97752fc41
유은지 <yu034254@gmail.com>
2025-08-05 12:43:12 +0900

    Fragment추가

--- front-server/app/src/main/java/com/example/talent_link/MainActivity.kt
+++ front-server/app/src/main/java/com/example/talent_link/MainActivity.kt
@@ -5,16 +5,62 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.talent_link.Favorite.FavoriteFragment
+import com.example.talent_link.Home.HomeFragment
+import com.example.talent_link.LocalLife.LocalLifeFragment
+import com.example.talent_link.Mypage.MyPageFragment
+import com.example.talent_link.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // 초기 화면 고정
+        supportFragmentManager.beginTransaction().
+        replace(R.id.frame, HomeFragment()).commit()
+
+
+        // 네비 버튼 연결
+        binding.nav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.btnHome->{
+                    supportFragmentManager.beginTransaction().
+                    replace(R.id.frame, HomeFragment()).commit()
+                }
+                R.id.btnFavorit->{
+                    supportFragmentManager.beginTransaction().
+                    replace(R.id.frame, FavoriteFragment()).commit()
+                }
+                R.id.btnLife->{
+                    supportFragmentManager.beginTransaction().
+                    replace(R.id.frame, LocalLifeFragment()).commit()
+                }
+
+                // chatFragment
+//                R.id.btnChat->{
+//                    supportFragmentManager.beginTransaction().
+//                    replace(R.id.frame, ()).commit()
+//                }
+
+                R.id.btnMyPage->{
+                    supportFragmentManager.beginTransaction().
+                    replace(R.id.frame, MyPageFragment()).commit()
+                }
+
+
+            }
+            true
+        }
+
+
     }
 }
